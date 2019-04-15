063ba83e5c15d0bf45035ccc447f74e65270fe59
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-15 16:00:07 +0300

    created route to navigate to the selected article

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2019-03-15&sortBy=source&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    news = requests.get(url).json().get("articles")
+    url = 'https://newsapi.org/v2/sources?apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    news = requests.get(url).json().get("sources")
     print(news)
     return render_template('index.html', news=news)
 
+
+@app.route('/article/<string:id>')  # example.com/hello/Anthony
+def article(id):
+    url = 'https://newsapi.org/v2/sources?id="abc-news"&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    print(requests.get(url).json())
+    return 'Hello ' + id + '!'  # returns hello Anthony!
