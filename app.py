c4b77b05a5d04adbc9b6da85f81bd464eee7d14b
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-15 12:53:35 +0300

    rendered article sources to landing page

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2019-03-15&sortBy=source&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
     news = requests.get(url).json().get("articles")
-
+    print(news)
     return render_template('index.html', news=news)
 
