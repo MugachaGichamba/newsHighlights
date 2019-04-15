fb26e33a5b6e33699ef749241a0523089e99d85e
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-15 12:28:08 +0300

    Rendering data from api to template done

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    r = requests.get(url)
-    print(r.json())
-    return render_template('index.html')
+    url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    news = requests.get(url).json().get("articles")
+
+    return render_template('index.html', news=news)
 
