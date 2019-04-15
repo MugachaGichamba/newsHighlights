74bc618ff8deea404bcc5865c4134c8eff5022b7
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-15 11:37:23 +0300

    successfully fetched data from news api and displayed it in the terminal

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    r = requests.get(url)
+    print(r.json())
+    return render_template('index.html')
 
