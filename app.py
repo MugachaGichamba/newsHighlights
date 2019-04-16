cd623a31973391fed05aa2ed72b8c4da3c3baa3f
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-16 06:04:34 +0300

    top headlines views and template done

--- app.py
+++ app.py
@@ -7,18 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://newsapi.org/v2/sources?apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    news = requests.get(url).json().get("sources")
+    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    news = requests.get(url).json().get("articles")
     print(news)
     return render_template('index.html', news=news)
-
-
-@app.route('/article/<string:id>')
-def article(id):
-    # print(id)
-    # # url = f'https://newsapi.org/v2/sources?id={id}&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    # print(requests.get('https://newsapi.org/v2/sources?id=mirror&apiKey=57aa9f751dd44bb096ccd3b24857c51b').json())
-    return 'Hello ' + id + '!'
 
 
 @app.route('/sources')
