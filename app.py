1e5c31b08b7a7f62996366807746310502120780
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-16 10:06:41 +0300

    successfully diaplayed sources from a particular id

--- app.py
+++ app.py
@@ -20,3 +20,11 @@ def sources():
     print(news)
     return render_template('sources.html', news=news)
 
+
+@app.route('/sources/<id>')
+def articles(id):
+    url = f'https://newsapi.org/v2/top-headlines?sources={id}&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    articles = requests.get(url).json().get("articles")
+    print(articles)
+    return render_template('article.html', articles=articles)
+
