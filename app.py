69b84906c253128c115bc4204ecea04fc17c20d0
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-16 10:28:25 +0300

    finalised styling and indentation styling

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    news = requests.get(url).json().get("articles")
-    print(news)
-    return render_template('index.html', news=news)
-
-
-@app.route('/sources')
 def sources():
     url = 'https://newsapi.org/v2/sources?apiKey=57aa9f751dd44bb096ccd3b24857c51b'
     news = requests.get(url).json().get("sources")
