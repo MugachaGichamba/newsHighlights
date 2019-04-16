8bb01b79c9d9e405615413fb703d6f80cab85283
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-16 05:34:40 +0300

    Added route to show all news soures

--- app.py
+++ app.py
@@ -15,7 +15,16 @@ def index():
 
 @app.route('/article/<string:id>')
 def article(id):
-    print(id)
-    # url = f'https://newsapi.org/v2/sources?id={id}&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
-    print(requests.get('https://newsapi.org/v2/sources?id=mirror&apiKey=57aa9f751dd44bb096ccd3b24857c51b').json())
+    # print(id)
+    # # url = f'https://newsapi.org/v2/sources?id={id}&apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    # print(requests.get('https://newsapi.org/v2/sources?id=mirror&apiKey=57aa9f751dd44bb096ccd3b24857c51b').json())
     return 'Hello ' + id + '!'
+
+
+@app.route('/sources')
+def sources():
+    url = 'https://newsapi.org/v2/sources?apiKey=57aa9f751dd44bb096ccd3b24857c51b'
+    news = requests.get(url).json().get("sources")
+    print(news)
+    return render_template('index.html', news=news)
+
